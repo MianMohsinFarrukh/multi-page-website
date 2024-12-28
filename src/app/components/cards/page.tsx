5ad03cc6fb701b37beca8d814c9ca7200981380d
MianMohsinFarrukh <1mohsinfarrukh@gmail.com>
2024-12-28 11:53:03 +0500

    update

--- src/app/components/cards/page.tsx
+++ src/app/components/cards/page.tsx
@@ -3,7 +3,9 @@ import React from 'react'
 export default function Cards() {
     return (
         <div>
-            <div className=''></div>
+            <div className=''>
+                <div></div>
+            </div>
         </div>
     );
 };
