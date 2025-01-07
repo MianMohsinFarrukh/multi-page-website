16b461b4310613190dc8068772677572418536a8
MianMohsinFarrukh <1mohsinfarrukh@gmail.com>
2025-01-07 11:22:02 +0500

    redeem-complete

--- src/app/pages/redeempage/page.tsx
+++ src/app/pages/redeempage/page.tsx
@@ -3,17 +3,12 @@ import SuggestedRewards from '@/app/components/secondcards/page'
 import React from 'react'
 import { FaSearch } from 'react-icons/fa'
 import { FiChevronDown } from 'react-icons/fi'
+import { MdOutlineSegment } from 'react-icons/md'
 
 export default function Redeempage() {
     return (
         <div className="min-h-screen bg-gray-100 py-6 ">
             <Homebar />
-
-
-
-
-
-
 
             <div className="flex flex-col lg:flex-row p-6 bg-gray-100 min-h-screen px-16">
                 {/* Left Side Panel */}
@@ -32,7 +27,7 @@ export default function Redeempage() {
                             <span>Show</span>
                             <FiChevronDown />
                         </div>
-                        <div className="text-gray-800 font-semibold">All categories</div>
+                        <div className="text-gray-800 font-semibold flex"><MdOutlineSegment className='mr-1' />All categories</div>
                     </div>
 
                     <ul className="space-y-4 text-gray-600">
@@ -43,68 +38,62 @@ export default function Redeempage() {
 
                 {/* Right Side Cards */}
                 <div>
-                <div className="w-full lg:w-3/2 grid grid-cols-1 md:grid-cols-2 gap-4 ml-4">
-                    {/* Card 1 */}
-                    <div className="bg-white rounded-md shadow-md p-4">
-                        <img
-                            src="/images/sea.png"
-                            alt="Sea of Thieves"
-                            className="w-full rounded-md mb-4"
-                        />
-                        <h3 className="text-lg font-semibold mb-2">Sea of Thieves: Ancient Coin Pack</h3>
-                        <p className="text-gray-600 mb-4">1,700 points</p>
-                        <button className="text-blue-500 font-medium">REDEEM REWARD</button>
+                    <div className="w-full lg:w-3/2 grid grid-cols-1 md:grid-cols-2 gap-4 ml-4">
+                        {/* Card 1 */}
+                        <div className="bg-white rounded-md shadow-md p-4">
+                            <img
+                                src="/images/sea.png"
+                                alt="Sea of Thieves"
+                                className="w-full rounded-md mb-4"
+                            />
+                            <h3 className="text-lg font-semibold mb-2">Sea of Thieves: Ancient Coin Pack</h3>
+                            <p className="text-gray-600 mb-4">1,700 points</p>
+                            <button className="text-blue-500 font-medium">REDEEM REWARD</button>
+                        </div>
+
+                        {/* Card 2 */}
+                        <div className="bg-white rounded-md shadow-md p-4">
+                            <img
+                                src="/images/sec.png"
+                                alt="Overwatch Coins"
+                                className="w-full rounded-md mb-4"
+                            />
+                            <h3 className="text-lg font-semibold mb-2">Overwatch Coins Digital Code</h3>
+                            <p className="text-gray-600 mb-4">1,800 points</p>
+                            <button className="text-blue-500 font-medium">REDEEM REWARD</button>
+
+                        </div>
+                        {/* Card 3 */}
+                        <div className="bg-white rounded-md shadow-md p-4">
+                            <img
+                                src="/images/the.png"
+                                alt="Sea of Thieves"
+                                className="w-full rounded-md mb-4"
+                            />
+                            <h3 className="text-lg font-semibold mb-2">Shopon.pk Gift Card Rs300</h3>
+                            <p className="text-gray-600 mb-4">1,700 points</p>
+                            <button className="text-blue-500 font-medium">REDEEM REWARD</button>
+                        </div>
+                        {/* Card 4 */}
+                        <div className="bg-white rounded-md shadow-md p-4">
+                            <img
+                                src="/images/the.png"
+                                alt="Sea of Thieves"
+                                className="w-full rounded-md mb-4"
+                            />
+                            <h3 className="text-lg font-semibold mb-2">Shopon.pk Gift Card Rs300</h3>
+                            <p className="text-gray-600 mb-4">1,700 points</p>
+                            <button className="text-blue-500 font-medium">REDEEM REWARD</button>
+                        </div>
+
                     </div>
 
-                    {/* Card 2 */}
-                    <div className="bg-white rounded-md shadow-md p-4">
-                        <img
-                            src="/images/sec.png"
-                            alt="Overwatch Coins"
-                            className="w-full rounded-md mb-4"
-                        />
-                        <h3 className="text-lg font-semibold mb-2">Overwatch Coins Digital Code</h3>
-                        <p className="text-gray-600 mb-4">1,800 points</p>
-                        <button className="text-blue-500 font-medium">REDEEM REWARD</button>
+                    <div className='bg-black h-1 w-full mt-10 md:ml-4'></div>
 
-                    </div>
-                    {/* Card 3 */}
-                    <div className="bg-white rounded-md shadow-md p-4">
-                        <img
-                            src="/images/the.png"
-                            alt="Sea of Thieves"
-                            className="w-full rounded-md mb-4"
-                        />
-                        <h3 className="text-lg font-semibold mb-2">Shopon.pk Gift Card Rs300</h3>
-                        <p className="text-gray-600 mb-4">1,700 points</p>
-                        <button className="text-blue-500 font-medium">REDEEM REWARD</button>
-                    </div>
-                     {/* Card 4 */}
-                     <div className="bg-white rounded-md shadow-md p-4">
-                        <img
-                            src="/images/the.png"
-                            alt="Sea of Thieves"
-                            className="w-full rounded-md mb-4"
-                        />
-                        <h3 className="text-lg font-semibold mb-2">Shopon.pk Gift Card Rs300</h3>
-                        <p className="text-gray-600 mb-4">1,700 points</p>
-                        <button className="text-blue-500 font-medium">REDEEM REWARD</button>
-                    </div>
-                   
+                    <div><SuggestedRewards /></div>
                 </div>
 
-                <div className='bg-black h-1 w-full '></div>
-
-
-
-
-<div><SuggestedRewards /></div>
             </div>
-
-            </div>
-
-
-
         </div>
     )
 }
